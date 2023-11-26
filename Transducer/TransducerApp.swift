a191f141a8531afabf9407a5f92a19da270fe0c7
Yoon <jiwookyoon0744@gmail.com>
2023-11-26 15:12:46 +0900

    Initial Commit

---
+++ Transducer/TransducerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TransducerApp.swift
+//  Transducer
+//
+//  Created by 윤지욱 on 11/26/23.
+//
+
+import SwiftUI
+
+@main
+struct TransducerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
