e15c398eaa117168c921fc4a0cdf37feaac14d10
Yoon <jiwookyoon0744@gmail.com>
2023-11-30 01:24:25 +0900

    UI Completed

--- Transducer/ContentView.swift
+++ Transducer/ContentView.swift
@@ -2,23 +2,63 @@
 //  ContentView.swift
 //  Transducer
 //
-//  Created by 윤지욱 on 11/26/23.
-//
 
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+    @State var source = ""
+    @State var destination = ""
+    @State var inputNumber = ""
+    @State var outputNumber = ""
+    var selections = ["인치","야드","마일","피트","미터","킬로미터","센티미터"]
+    
+    var calculation:Double {
+        
     }
-}
-
-#Preview {
-    ContentView()
+    
+    var body: some View {
+        VStack (alignment: .center){
+            Text("영미 단위 변환기")
+                .font(.title)
+                .padding()
+            
+            TextField("변환할 숫자를 입력하세요", text: $inputNumber)
+                .padding(.leading ,55)
+                .keyboardType(.decimalPad)
+            
+            HStack{
+                Picker("입력 단위", selection: $source) {
+                    ForEach(selections, id: \.self){
+                        Text($0)
+                    }
+                }
+                .pickerStyle(.menu)
+                .padding()
+                
+                Text("➞")
+                    .font(.title2)
+                    .padding()
+                
+                Picker("입력 단위", selection: $destination) {
+                    ForEach(selections, id: \.self){
+                        Text($0)
+                    }
+                }
+                .pickerStyle(.menu)
+                .padding()
+            }
+            
+            Text(outputNumber)
+                .font(.largeTitle)
+                .padding()
+            
+            .padding()
+        }
+    }
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
 }
