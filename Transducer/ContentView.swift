d58c6abd018e3d26d091323d163863c6360ca331
Yoon <jiwookyoon0744@gmail.com>
2023-11-30 16:16:37 +0900

    release candidate 2

--- Transducer/ContentView.swift
+++ Transducer/ContentView.swift
@@ -6,20 +6,20 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var source = "인치"
-    @State var destination = "인치"
-    @State var inputNumber = 0.0
-    @State var outputNumber = 0.0
-    @State var meter = 0.0
-    var selections = ["인치","야드","마일","피트","미터","킬로미터","센티미터"]
+    @State var source = "피트" //입력 단위
+    @State var destination = "미터" //출력 단위
+    @State var inputNumber = 0.0 //입력 숫자
+    @State var outputNumber = 0.0 //출력 숫자
+    @State var meter = 0.0 //미터값
+    var selections = ["인치","야드","마일","피트","미터","킬로미터","센티미터"] //단위 선택지
     
-    let formatter: NumberFormatter = {
+    let formatter: NumberFormatter = { //텍스트필드 형식 지정 (Double)
             let formatter = NumberFormatter()
             formatter.numberStyle = .decimal
             return formatter
     }()
     
-    func calculation1(inputnumber: Double) -> Double {
+    func calculation1(inputnumber: Double) -> Double { //모든 단위를 미터로 변환
         switch source {
         case "인치":
             calculation2(meter:inputNumber * 0.0254)
@@ -38,7 +38,7 @@ struct ContentView: View {
         }
     }
     
-    func calculation2(meter: Double) -> Double {
+    func calculation2(meter: Double) -> Double { //미터에서 목적 단위로 변환
         if source == destination{
             return inputNumber
         }
@@ -60,18 +60,19 @@ struct ContentView: View {
         }
     }
     
-    var body: some View {
+    var body: some View { //UI 영역
         VStack (alignment: .center){
-            Text("영미 단위 변환기")
+            Text("영미 단위 변환기") //타이틀
                 .font(.title)
                 .padding()
             
-            TextField("변환할 숫자를 입력하세요", value: $inputNumber, formatter: formatter)
-                .padding(.leading ,55)
+            TextField("변환할 숫자를 입력하세요", value: $inputNumber, formatter: formatter) //숫자 입력 필드
+                .background(Color(uiColor: .secondarySystemBackground))
+                .padding()
                 .keyboardType(.decimalPad)
             
             HStack{
-                Picker("입력 단위", selection: $source) {
+                Picker("입력 단위", selection: $source) { //입력 단위 선택
                     ForEach(selections, id: \.self){
                         Text($0)
                     }
@@ -80,11 +81,11 @@ struct ContentView: View {
                 .pickerStyle(.menu)
                 .padding()
                 
-                Text("➞")
+                Text("➞") //화살표
                     .font(.title2)
                     .padding()
                 
-                Picker("입력 단위", selection: $destination) {
+                Picker("출력 단위", selection: $destination) { //출력 단위 선택
                     ForEach(selections, id: \.self){
                         Text($0)
                     }
@@ -94,13 +95,13 @@ struct ContentView: View {
                 .padding()
             }
             
-            Text(String(format: "%.2f", calculation1(inputnumber: inputNumber)))
+            Text(String(format: "%.2f", calculation1(inputnumber: inputNumber))) //결과 필드
                 .font(.largeTitle)
                 .padding()
         }
     }
     
-    struct ContentView_Previews: PreviewProvider {
+    struct ContentView_Previews: PreviewProvider { //SwiftUI 기본 구문 (수정X)
         static var previews: some View {
             ContentView()
         }
